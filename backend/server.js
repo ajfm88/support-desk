eb022773f04f3255ec8b44c46ea8c009641fb25a
ajfm88 <ajfoucaultmo@mail.usf.edu>
2022-05-03 11:52:35 -0700

    14. Logout User

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 // Connect to database
 connectDB();
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.status(201).json({ message: "Welcome to the Support Desk API" });
+  res.status(200).json({ message: "Welcome to the Support Desk API" });
 });
 
 // Routes
